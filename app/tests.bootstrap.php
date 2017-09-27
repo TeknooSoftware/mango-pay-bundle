f6402ccbc5ec231084fc5251be3344b70eecabae
Richard Deloge <richarddeloge@gmail.com>
2017-09-27 22:52:12 +0200

    Test fix doctrine without annotations

--- app/tests.bootstrap.php
+++ app/tests.bootstrap.php
@@ -26,6 +26,5 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
