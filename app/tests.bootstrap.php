121fb2d213241b44f8d9c3e05dcc98a9f294b6c3
Richard Deloge <richarddeloge@gmail.com>
2017-09-27 23:24:34 +0200

    rollback

--- app/tests.bootstrap.php
+++ app/tests.bootstrap.php
@@ -26,5 +26,6 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
